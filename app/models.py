7d3ab3a5d516e03eb13452ae7cbbf534fe416bea
Antônio Antunes <antunesantonio91@gmail.com>
2024-08-23 09:51:39 -0300

    adicionando models nos templates

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tarefa(models.Model):
+    nome = models.CharField(max_length=100)
+    status = models.CharField(max_length=20)
+    prazo = models.DateField()
+    
+    def __str__(self):
+        return self.nome 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from datetime import date
+from .models import Tarefa
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    hoje = date.today()
+
+    tarefas = Tarefa.objects.all()
+
+    context = {
+        "tarefas": tarefas,
+        "hoje": hoje, 
+    }
+
+    return render(request, 'index.html', context)
